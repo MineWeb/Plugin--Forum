a2d0665bb4998366b901041513af2d4970ffc4c6
Thibault <thibault@stantabcorp.com>
2021-03-06 17:15:29 +0100

    feat: Html Purifier

--- Controller/Component/ForumSecurityComponent.php
+++ Controller/Component/ForumSecurityComponent.php
@@ -4,8 +4,12 @@ class ForumSecurityComponent extends Component
 {
     public static function removeScript($content)
     {
-        require "../Plugin/Forum/Core/lib/htmLawed/htmLawed.php";
-
-        return htmLawed($content, ['safe' => 1]);
+//        require "../Plugin/Forum/Core/lib/htmLawed/htmLawed.php";
+//
+//        return htmLawed($content, ['safe' => 1]);
+        require __DIR__ . "/../../Core/lib/htmlpurifier/library/HTMLPurifier.auto.php";
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($content);
     }
 }
